9fe11bc25385174be9ea9685328e450207d0c628
jxlee423 <elileejiaxin@gmail.com>
2024-09-22 01:56:05 +0800

    Delete datasets/synthetic/Latest_Code/test_gtriescanner.m

--- datasets/synthetic/Latest_Code/test_gtriescanner.m
+++
@@ -1,7 +0,0 @@
-function run_external_program(input_file, output_file_base)
-    gtrieScanner_mex(input_file, output_file_base);
-end
-
-input_file = 'G:\2024 Summer\EFHT\Synthetic Data Sets\example_graph.txt';
-output_file_base = 'G:\2024 Summer\EFHT\Synthetic Data Sets\output_base';
-run_external_program(input_file, output_file_base);
