da1f4947c81906570ca75598a5d1b302ec583b2a
jxlee423 <elileejiaxin@gmail.com>
2025-07-11 04:03:29 +0800

    move config.py to src, and add relative path

--- EFHT - Analysis/src/config.py
+++ EFHT - Analysis/src/config.py
@@ -1,18 +1,9 @@
-# config.py
 
 # --- Dataset Configuration ---
-# Modify this ID before running a new dataset
-DATASET_ID = ""
-
-# Input file path
-INPUT_FILE_PATH = ""
-
-# --- Output Configuration ---
-# Root directory for results reports
 OUTPUTS_DIR = "outputs"
 
 # --- Test Parameters ---
-# Significance level parameter
+# General Parameters
 ALPHA = 0.05
 EPSILON = 1e-10
 RANDOM_STATE = 423
@@ -21,6 +12,8 @@ RANDOM_STATE = 423
 INDEPENDENCE_N_SIMULATIONS = 1000
 INDEPENDENCE_TOP_N_COLORS = 3
 INDEPENDENCE_GRID_SIZE = 1000
+INDEPENDENCE_EXTEND_RATIO = 0.2
+
 # Bivariate Equivalence Test Parameters
 BIVARIATE_N_PERMUTATIONS = 1000
 BIVARIATE_GRID_SIZE = 1000
