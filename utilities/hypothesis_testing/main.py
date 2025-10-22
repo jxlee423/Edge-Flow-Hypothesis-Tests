f87bdcf8b07e0184f996b0b996f9755e02698e0c
jxlee423 <elileejiaxin@gmail.com>
2025-10-23 04:09:53 +0800

    Add graph type option for stochastic block

--- utilities/hypothesis_testing/main.py
+++ utilities/hypothesis_testing/main.py
@@ -12,7 +12,7 @@ from utilities.hypothesis_testing import data_preprocess
 from utilities.hypothesis_testing.tests import ks, bivariate, independence
 import os
 
-def main(input_path, dataset_id, batch_id, output_dir, run_ks, run_independence, run_bivariate, jobs):
+def main(input_path, dataset_id, batch_id, output_dir, run_ks, run_independence, run_bivariate, jobs, graph_type='SmallWorld'):
     # --- 1. Initialization ---
     print(f"Starting EFHT analysis - Batch ID: {batch_id}, Dataset ID: {dataset_id}")
     results = ResultsManager(output_dir, batch_id, dataset_id, input_path)
@@ -22,8 +22,11 @@ def main(input_path, dataset_id, batch_id, output_dir, run_ks, run_independence,
     df_input = pd.read_csv(input_path)
 
     if run_ks or run_bivariate:
-        print("   - Running Classifying...")
-        class_data = data_preprocess.Classifying(df_input)
+        print(f"   - Running Classifying (using {graph_type} graph)...")
+        if graph_type == 'SmallWorld':
+            class_data = data_preprocess.Classifying_SW(df_input)
+        elif graph_type == 'StochasticBlock':
+            class_data = data_preprocess.Classifying_SB(df_input)
 
         # Save the result of Classifying
         class_data_path = os.path.join(results.run_dir, f"{dataset_id}_preprocessed_class_data.csv")
@@ -114,5 +117,6 @@ if __name__ == "__main__":
     parser.add_argument('--no-independence', dest='run_independence', action='store_false', help='Do not run the Independence test.')
     parser.add_argument('--no-bivariate', dest='run_bivariate', action='store_false', help='Do not run the Bivariate Equivalence test.')
     parser.add_argument('--jobs', type=int, default=-1, help='Number of parallel jobs for data preprocessing. -1 uses all available cores (default).')
+    parser.add_argument('--graph-type', type=str, default='SmallWorld', choices=['SmallWorld', 'StochasticBlock'], help="Type of graph model for classification. 'SmallWorld' uses Classifying_SM, 'StochasticBlock' uses Classifying_SB. Default is 'smallworld'.")
     args = parser.parse_args()
-    main(args.input, args.id, args.batch_id, args.output, args.run_ks, args.run_independence, args.run_bivariate, args.jobs)
+    main(args.input, args.id, args.batch_id, args.output, args.run_ks, args.run_independence, args.run_bivariate, args.jobs, args.graph_type)
